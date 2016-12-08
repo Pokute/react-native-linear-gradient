51adfbb4c3f3ffcabd2f28774ef64894d14fda90
Eero Tanskanen <eero.tanskanen@haaveinc.com>
2016-12-08 13:09:26 +0200

    Added initial stopgap version for windows. Just uses first color in colors[] for whole element without any actual gradient.

---
+++ index.windows.js
@@ -0,0 +1,42 @@
+import React, { Component, PropTypes } from 'react';
+import { processColor, requireNativeComponent, View } from 'react-native';
+
+type PropsType = {
+  start?: Array<number>;
+  end?: Array<number>;
+  colors: Array<string>;
+  locations?: Array<number>;
+} & typeof(View);
+
+export default class LinearGradient extends Component {
+  static propTypes = {
+    start: PropTypes.arrayOf(PropTypes.number),
+    end: PropTypes.arrayOf(PropTypes.number),
+    colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    locations: PropTypes.arrayOf(PropTypes.number),
+    ...View.propTypes,
+  };
+  props: PropsType;
+  gradientRef: any;
+
+  render() {
+    const {
+      colors,
+      locations,
+      ...otherProps
+    } = this.props;
+    if ((colors && locations) && (colors.length !== locations.length)) {
+      console.warn('LinearGradient colors and locations props should be arrays of the same length');
+    }
+
+    return (
+      <View
+        ref={(component) => { this.gradientRef = component; }}
+        {...otherProps}
+        style={[otherProps.style ? otherProps.style : {}, colors.length > 0 ? { backgroundColor: colors[0] } : {} ]}
+      />
+    );
+  }
+}
+
+const NativeLinearGradient = requireNativeComponent('BVLinearGradient', null);
